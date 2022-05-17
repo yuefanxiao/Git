9372dfd6b17d6c7cbb71c6121335898967cb2217
yuefanxiao <591649725@qq.com>
2022-05-17 18:00:08 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yuefanxiao
+ * @create 2022-05-17 17:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
