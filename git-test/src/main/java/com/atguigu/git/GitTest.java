4fa71dc49875b48fd91dd2a9cba4a27f9e1b0e4b
yuefanxiao <74059346+yuefanxiao@users.noreply.github.com>
2022-05-17 21:49:35 +0800

    pull commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test2!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
